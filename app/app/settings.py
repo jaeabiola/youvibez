3cbf49e8ab1b7f1edebef8fc656f2046b07ec059
PeteNelson22 <peternelson063@gmail.com>
2023-02-21 22:01:54 +0100

    Fixed bug in songs.html

--- app/app/settings.py
+++ app/app/settings.py
@@ -55,6 +55,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+
+    
 ]
 
 ROOT_URLCONF = 'app.urls'
